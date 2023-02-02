97effda81dc80278a3c549efb467ca51c3f56617
Radostin Ivanov <radipleven@abv.bg>
2023-02-02 10:46:09 +0200

    Update LinkedList.cpp

--- Linked List/LinkedList.cpp
+++ Linked List/LinkedList.cpp
@@ -1,1 +1,11 @@
+template <class T>
+LinkedList<T>::LinkedList() : front(nullptr), size(0) {}
 
+template <class T>
+LinkedList<T>::~LinkedList() {
+    while (front != nullptr) {
+        Node<T> *temp = front;
+        front = front->next;
+        delete temp;
+    }
+}
