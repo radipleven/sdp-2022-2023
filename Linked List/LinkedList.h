ebe3db5e4f07d2024ae0ab0fe05ba66c1630c3e3
Radostin Ivanov <radipleven@abv.bg>
2023-02-02 10:45:30 +0200

    Update LinkedList.h

--- Linked List/LinkedList.h
+++ Linked List/LinkedList.h
@@ -1,1 +1,15 @@
-
+template <class T>
+class LinkedList {
+    private:
+        template <class G>
+        struct Node {
+            G key;
+            Node<G>* next;
+            Node(G key) : key(key), next(nullptr){};
+        };
+        Node<T>* front;
+        size_t size;
+    public:
+        LinkedList();
+        ~LinkedList();
+};
